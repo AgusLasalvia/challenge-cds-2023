37b060a99b21e1e93a74f50ba7843609b3ec2121
AgusLasalvia <agus.blumenfeld13@gmail.com>
2025-11-19 16:22:22 -0300

    format: movie.routes.ts

--- src/routes/movie.routes.ts
+++ src/routes/movie.routes.ts
@@ -1,5 +1,6 @@
 import { Router } from "express";
 import { isUserLoggedIn } from "../middlewares/login.middleware.js";
+
 import MovieController from "../controllers/movie.controller.js";
 
 const router: Router = Router();
